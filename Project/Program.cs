5283b00d057afa5ad032569ec7f89d83990916de
Lasy <47871724+lramovha@users.noreply.github.com>
2024-11-01 00:04:47 +0200

    MainForm
    Main form with reference to the Reservation, Guests, and Rooms forms

--- Project/Program.cs
+++ Project/Program.cs
@@ -18,7 +18,7 @@ namespace Project
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new ReservationForm());
+            Application.Run(new MainForm());
         }
     }
 }
--- Project/presentation/MainForm.cs
+++ Project/presentation/MainForm.cs
@@ -17,9 +17,22 @@ namespace Project.presentation
             InitializeComponent();
         }
 
+        private void guestsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            GuestForm guestForm = new GuestForm();
+            guestForm.Show(); // Opens the GuestForm
+        }
+
         private void roomsToolStripMenuItem_Click(object sender, EventArgs e)
         {
+            RoomForm roomForm = new RoomForm();
+            roomForm.Show(); // Opens the RoomForm
+        }
 
+        private void reservationsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ReservationForm reservationForm = new ReservationForm();
+            reservationForm.Show(); // Opens the ReservationForm
         }
     }
 }
